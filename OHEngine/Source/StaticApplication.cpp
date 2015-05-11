33b802ca0deef055b06e9219bbd78bfe51b684e1
MonkeyD-IchiJou <ichi_jou@hotmail.com>
2015-05-12 02:08:47 +0800

    delete old application file

--- OHEngine/Source/StaticApplication.cpp
+++
@@ -1,9 +0,0 @@
-/******************************************************************************/
-/*!
-\file	StaticApplication.cpp
-\author Chuk Yih Jou
-\brief
-creating window and init opengl here, hardware abstraction input also done here
-*/
-/******************************************************************************/
-
